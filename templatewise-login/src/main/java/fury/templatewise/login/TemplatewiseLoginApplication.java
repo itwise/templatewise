3ae2c14e42439b11a3da5ba766ad2072936114b2
jdseo <jdeok.seo@gmail.com>
2015-08-19 15:10:56 +0900

    프로젝트 초기 구성 (index 페이지 띄우기)

--- templatewise-login/src/main/java/fury/templatewise/login/TemplatewiseLoginApplication.java
+++ templatewise-login/src/main/java/fury/templatewise/login/TemplatewiseLoginApplication.java
@@ -1,5 +1,24 @@
 package fury.templatewise.login;
 
-public class TemplatewiseLoginApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
+@SpringBootApplication
+@PropertySource({"classpath:/config/properties/${spring.profiles.active}/application-${spring.profiles.active}.properties"})
+public class TemplatewiseLoginApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TemplatewiseLoginApplication.class);
+	}
+	
+	@Profile("local")
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatewiseLoginApplication.class, args);
+	}
+	
 }
